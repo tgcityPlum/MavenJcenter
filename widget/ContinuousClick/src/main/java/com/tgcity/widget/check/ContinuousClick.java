6ee7b768d330c4cd561479f88d4fc7bced237dee
chenwei <wei.chen@uzer.me>
2019-12-12 17:07:38 +0800

    add: 添加ContinuousClick模块

---
+++ widget/ContinuousClick/src/main/java/com/tgcity/widget/check/ContinuousClick.java
@@ -0,0 +1,43 @@
+package com.tgcity.widget.check;
+
+/**
+ * @author TGCity
+ * 连点的校验
+ */
+
+public class ContinuousClick {
+    private static LimitQueue<OneClick> queue = new LimitQueue<>(20);
+
+    public static boolean check(Object o) {
+        String flag;
+        if(o == null) {
+            flag = Thread.currentThread().getStackTrace()[2].getMethodName();
+        } else {
+            flag = o.toString();
+        }
+        for (OneClick util : queue.getArrayList()) {
+            if (util.getMethodName().equals(flag)) {
+                return util.check();
+            }
+        }
+        OneClick clickUtil = new OneClick(flag);
+        queue.offer(clickUtil);
+        return clickUtil.check();
+    }
+    public static boolean check400(Object o) {
+        String flag;
+        if(o == null) {
+            flag = Thread.currentThread().getStackTrace()[2].getMethodName();
+        } else {
+            flag = o.toString();
+        }
+        for (OneClick util : queue.getArrayList()) {
+            if (util.getMethodName().equals(flag)) {
+                return util.check400();
+            }
+        }
+        OneClick clickUtil = new OneClick(flag);
+        queue.offer(clickUtil);
+        return clickUtil.check400();
+    }
+}
