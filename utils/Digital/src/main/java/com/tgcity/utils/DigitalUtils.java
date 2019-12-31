71ae4323a6add03777673b2d91ebc33589cea61c
chenwei <wei.chen@uzer.me>
2019-12-31 10:34:12 +0800

    add: Digital工具类

---
+++ utils/Digital/src/main/java/com/tgcity/utils/DigitalUtils.java
@@ -0,0 +1,17 @@
+package com.tgcity.utils;
+
+/**
+ * @author TGCity
+ * @date 2019/12/31
+ * @description 数字工具类
+ */
+public class DigitalUtils {
+    public static final int LEVEL_0 = 0;
+    public static final int LEVEL_1 = 1;
+    public static final int LEVEL_2 = 2;
+    public static final int LEVEL_3 = 3;
+    public static final int LEVEL_4 = 4;
+    public static final int LEVEL_6 = 6;
+    public static final int LEVEL_11 = 11;
+    public static final int LEVEL_16 = 16;
+}
