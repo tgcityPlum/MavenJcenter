8e0b42fca2362a3f1c1c66125b0718b33e871a3f
chenwei <wei.chen@uzer.me>
2020-01-22 13:08:49 +0800

    update: 升级MVP功能模块1.0.2

--- profession/MVP/src/main/java/com/tgcity/profession/mvp/present/BasePresenter.java
+++ profession/MVP/src/main/java/com/tgcity/profession/mvp/present/BasePresenter.java
@@ -1,11 +1,11 @@
 package com.tgcity.profession.mvp.present;
 
-import com.tgcity.function.interfaces.OnPublicRefreshViewCallBack;
+import com.tgcity.profession.mvp.view.BaseView;
 
 /**
  * @author TGCity
  */
 
-public interface BasePresenter<V extends OnPublicRefreshViewCallBack> {
+public interface BasePresenter<V extends BaseView> {
 
 }
---
+++ profession/MVP/src/main/java/com/tgcity/profession/mvp/view/BaseView.java
@@ -0,0 +1,13 @@
+package com.tgcity.profession.mvp.view;
+
+import com.tgcity.function.interfaces.OnContextCallBack;
+import com.tgcity.function.interfaces.OnPublicRefreshViewCallBack;
+
+/**
+ * @author TGCity
+ * @date 2020/1/22
+ * @description
+ */
+public interface BaseView extends OnPublicRefreshViewCallBack, OnContextCallBack {
+
+}
