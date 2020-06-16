3ef2bb2184e10e0b0f33272466c1577663aa8781
chenwei <wei.chen@uzer.me>
2020-06-16 17:08:17 +0800

    change: 升级System版本1.0.3

--- utils/System/src/main/java/com/tgcity/utils/SystemUtils.java
+++ utils/System/src/main/java/com/tgcity/utils/SystemUtils.java
@@ -1,11 +1,17 @@
 package com.tgcity.utils;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
 
 import java.util.List;
 
@@ -150,4 +156,89 @@ public class SystemUtils {
         }
         return false;
     }
+
+    public static String getDeviceId(Context context) {
+        StringBuilder deviceId = new StringBuilder();
+        // 渠道标志
+        deviceId.append("a-");
+        try {
+            //IMEI（imei）
+            TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            @SuppressLint({"MissingPermission", "HardwareIds"})
+            String imei = tm.getDeviceId();
+            if (!TextUtils.isEmpty(imei)) {
+                deviceId.append("imei-");
+                deviceId.append(imei);
+                return deviceId.toString();
+            }
+            //序列号（sn）
+            @SuppressLint({"MissingPermission", "HardwareIds"})
+            String sn = tm.getSimSerialNumber();
+            if (!TextUtils.isEmpty(sn)) {
+                deviceId.append("sn-");
+                deviceId.append(sn);
+                return deviceId.toString();
+            }
+            //如果上面都没有， 则生成一个id：随机码
+            String uuid = getUUID(context);
+            if (!TextUtils.isEmpty(uuid)) {
+                deviceId.append("id-");
+                deviceId.append(uuid);
+                return deviceId.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            deviceId.append("id-");
+            deviceId.append(getUUID(context));
+        }
+        return deviceId.toString();
+    }
+
+    /**
+     * 得到全局唯一UUID
+     */
+    private static String getUUID(Context context) {
+        String androidId = "" + android.provider.Settings.Secure.getString(context.getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+
+        return androidId;
+    }
+
+    /**
+     * 获取设备厂商
+     */
+    public static String getBrand() {
+        return Build.BRAND;
+    }
+
+    /**
+     * 获取设备名称
+     */
+    public static String getDeviceName() {
+        return Build.MODEL;
+    }
+
+    /**
+     * 获取卡1手机号
+     */
+    public static String getCard1Number(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        @SuppressLint({"MissingPermission", "HardwareIds"})
+        String phoneN = tm.getLine1Number();
+        return phoneN;
+    }
+
+    /**
+     * 获取设备android api 版本号
+     */
+    public static int getDeviceApiLevel() {
+        return android.os.Build.VERSION.SDK_INT;
+    }
+
+    /**
+     * 获取设备android api 名称
+     */
+    public static String getDeviceApiName() {
+        return android.os.Build.VERSION.RELEASE;
+    }
+
 }
