7f4497bdafb38eada5689efdaee34594e144c75f
chenwei <wei.chen@uzer.me>
2020-06-29 10:43:39 +0800

    change: 升级ImagePicker版本1.0.3

---
+++ profession/ImagePicker/src/main/java/com/tgcity/profession/imagepicker/view/CustomGridView.java
@@ -0,0 +1,31 @@
+package com.tgcity.profession.imagepicker.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * @author jeremy
+ * @date 2019/5/30
+ * @des
+ */
+public class CustomGridView extends GridView {
+    public CustomGridView(Context context) {
+        super(context);
+    }
+
+    public CustomGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
