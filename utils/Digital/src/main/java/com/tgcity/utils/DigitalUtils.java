7ea10d5d37417263352c109e628e292f0ee5eaa7
chenwei <wei.chen@uzer.me>
2020-07-20 13:50:12 +0800

    change: 升级Digital版本1.0.11

--- utils/Digital/src/main/java/com/tgcity/utils/DigitalUtils.java
+++ utils/Digital/src/main/java/com/tgcity/utils/DigitalUtils.java
@@ -8,6 +8,7 @@ package com.tgcity.utils;
 public class DigitalUtils {
     public static final int LEVEL_N_1 = -1;
     public static final int LEVEL_0 = 0;
+    public static final float LEVEL_0_POINT_5 = 0.5f;
     public static final int LEVEL_1 = 1;
     public static final int LEVEL_2 = 2;
     public static final int LEVEL_3 = 3;
@@ -15,13 +16,23 @@ public class DigitalUtils {
     public static final int LEVEL_5 = 5;
     public static final int LEVEL_6 = 6;
     public static final int LEVEL_8 = 8;
+    public static final int LEVEL_9 = 9;
     public static final int LEVEL_10 = 10;
     public static final int LEVEL_11 = 11;
+    public static final int LEVEL_12 = 12;
     public static final int LEVEL_14 = 14;
+    public static final int LEVEL_15 = 15;
     public static final int LEVEL_16 = 16;
     public static final int LEVEL_20 = 20;
+    public static final int LEVEL_30 = 30;
     public static final int LEVEL_60 = 60;
+    public static final int LEVEL_90 = 90;
+    public static final int LEVEL_100 = 100;
+    public static final int LEVEL_105 = 105;
+    public static final int LEVEL_120 = 120;
+    public static final int LEVEL_135 = 135;
     public static final int LEVEL_401 = 401;
+    public static final int LEVEL_500 = 500;
 
     public static final int REQUEST_CODE_PERMISSION = 1000;
     public static final int REQUEST_CODE_LOCATION = 1001;
